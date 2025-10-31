708001702ac84e188295a6e41f09a738b73c0455
surendra <codingstar450@gmail.com>
2025-10-31 06:58:39 -0700

    add

---
+++ main.js
@@ -0,0 +1,104 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+const closeIcon = document.getElementById('nav-close');
+const navLink = document.querySelectorAll('.nav_link');
+navLink.forEach(link =>
+    link.addEventListener("click" ,()  =>
+    {
+    navMenu.classList.add('hidden')
+    
+    })
+)
+
+closeIcon.addEventListener("click",() => {
+    navMenu.classList.add('hidden')
+})
+
+hamburger.addEventListener("click",() => {
+    navMenu.classList.remove('hidden')
+})
+
+// <!--tabs-->
+const tabs =document.querySelectorAll(".tabs_wrap ul li");
+const all =document.querySelectorAll(".item_wrap");
+const  foods =document.querySelectorAll(".food");
+const beverages =document.querySelectorAll(".beverage");
+const snacks =document.querySelectorAll(".snack");
+
+  
+tabs.forEach(tab => {
+    tab.addEventListener ("click " , () => {
+        tabs.forEach( tab =>{
+            tab.classList.remove("active")
+        })
+        tab.classList.add('active')
+        const tabval =tab.getAttribute('data-tabs')
+
+        all.forEach( item => {
+            item.style.display ='none'
+        })
+
+
+
+        if(tabval == 'food') {
+            foods.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+          else  if (tabval == 'snack') {
+                snacks.forEach(item => {
+                    item.style.display = 'block'
+                })
+        }
+
+      else if(tabval == 'beverage') {
+            beverages.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+           else {
+                all.forEach(item => {
+                    item.style.display = 'block'
+                })
+            }
+            
+    })
+})
+
+// <!--dark Mose -->
+
+const html =document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+if(localStorage.getItem("mode") == "dark" )
+{
+    darkMode();
+
+}
+else{
+    lightMode();
+}
+themeBtn.addEventListener('click' , (e)  => {
+    if(localStorage.getItem("mode") == "light"){
+        darkMode();
+
+    }
+    else{
+        lightMode();
+    }
+})
+
+function lightMode() {
+    html.classList.remove("dark");
+    themeBtn.classList.replace("ri-sun-line","ri-moon-line");
+    localStorage.setItem("mode" ,"light");
+}
+
+function darkMode()  {
+    html.classList.add("dark");
+    themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+    localStorage.setItem("mode" ,"dark");
+}
+
+
+
